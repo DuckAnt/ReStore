b9a3a3d0ca969fe0a0674e03320874458d6d7936
DUCANH\louis <nguduckant@gmail.com>
2024-12-14 21:58:17 +0700

    End of session 3

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<StoreContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 // Add authorization services
 builder.Services.AddAuthorization();
 
@@ -26,6 +28,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+});
 
 app.UseAuthorization();
 
